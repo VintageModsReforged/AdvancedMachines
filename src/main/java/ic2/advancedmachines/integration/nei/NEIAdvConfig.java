ad4e606ea1ef0d76f936002c48370f30d4962e17
cross <cross.wanted.1.4@gmail.com>
2024-11-03 14:38:02 +0900

    added electrolyzer compat for nei

--- src/main/java/ic2/advancedmachines/integration/nei/NEIAdvConfig.java
+++ src/main/java/ic2/advancedmachines/integration/nei/NEIAdvConfig.java
@@ -4,13 +4,11 @@ import codechicken.nei.api.API;
 import codechicken.nei.api.IConfigureNEI;
 import ic2.advancedmachines.AdvancedMachines;
 import ic2.advancedmachines.integration.nei.providers.NEICompressorProvider;
+import ic2.advancedmachines.integration.nei.providers.NEIElectrolyzerProvider;
 import ic2.advancedmachines.integration.nei.providers.NEIExtractorProvider;
 import ic2.advancedmachines.integration.nei.providers.NEIMaceratorProvider;
 import ic2.advancedmachines.utils.Refs;
-import ic2.core.gui.GuiAdvCompressor;
-import ic2.core.gui.GuiAdvExtractor;
-import ic2.core.gui.GuiAdvInduction;
-import ic2.core.gui.GuiAdvMacerator;
+import ic2.core.gui.*;
 
 public class NEIAdvConfig implements IConfigureNEI {
 
@@ -25,9 +23,13 @@ public class NEIAdvConfig implements IConfigureNEI {
         API.registerRecipeHandler(new NEIExtractorProvider());
         API.registerUsageHandler(new NEIExtractorProvider());
 
+        API.registerRecipeHandler(new NEIElectrolyzerProvider());
+        API.registerUsageHandler(new NEIElectrolyzerProvider());
+
         API.registerGuiOverlay(GuiAdvMacerator.class, "macerator", 5, 11);
         API.registerGuiOverlay(GuiAdvCompressor.class, "compressor", 5, 11);
         API.registerGuiOverlay(GuiAdvExtractor.class, "extractor", 5, 11);
+        API.registerGuiOverlay(GuiAdvElectrolyzer.class, "electrolyzer", 0, 0);
         API.registerGuiOverlay(GuiAdvInduction.class, "smelting", -4, 11);
     }
 
---
+++ src/main/java/ic2/advancedmachines/integration/nei/providers/NEIElectrolyzerProvider.java
@@ -0,0 +1,126 @@
+package ic2.advancedmachines.integration.nei.providers;
+
+import codechicken.nei.NEIServerUtils;
+import codechicken.nei.PositionedStack;
+import codechicken.nei.forge.GuiContainerManager;
+import codechicken.nei.recipe.TemplateRecipeHandler;
+import ic2.advancedmachines.utils.AdvMachinesRecipeManager;
+import ic2.advancedmachines.utils.Refs;
+import ic2.core.gui.GuiAdvElectrolyzer;
+import net.minecraft.item.ItemStack;
+import org.lwjgl.opengl.GL11;
+
+import java.awt.*;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+@SuppressWarnings({"unchecked", "rawtypes"})
+public class NEIElectrolyzerProvider extends TemplateRecipeHandler {
+
+    int ticks;
+
+    @Override
+    public String getGuiTexture() {
+        return Refs.getGuiPath("charged");
+    }
+
+    @Override
+    public String getRecipeName() {
+        return "Electrolyzer";
+    }
+
+    @Override
+    public Class getGuiClass() {
+        return GuiAdvElectrolyzer.class;
+    }
+
+    @Override
+    public void drawBackground(GuiContainerManager gui, int recipe) {
+        GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
+        gui.bindTextureByName(this.getGuiTexture());
+        gui.drawTexturedModalRect(48, 18, 53, 34, 76, 18);
+    }
+
+    @Override
+    public void drawExtras(GuiContainerManager gui, int recipe) {
+        float f = this.ticks >= 20 ? (float) ((this.ticks - 20) % 20) / 20.0F : 0.0F;
+        this.drawProgressBar(gui, 74, 18, 176, 14, 24, 17, f, 0);
+    }
+
+    @Override
+    public void onUpdate() {
+        super.onUpdate();
+        ++this.ticks;
+    }
+
+    @Override
+    public void loadTransferRects() {
+        this.transferRects.add(new TemplateRecipeHandler.RecipeTransferRect(new Rectangle(70, 23, 32, 17), "ic2.electrolyzer"));
+    }
+
+    @Override
+    public void loadCraftingRecipes(String outputId, Object... results) {
+        if (outputId.equals("ic2.electrolyzer")) {
+            List recipes = AdvMachinesRecipeManager.getDrainElectrolyzerRecipes();
+            recipes.addAll(AdvMachinesRecipeManager.getPowerElectrolyzerRecipes());
+            Iterator recipesIter = recipes.iterator();
+            while (recipesIter.hasNext()) {
+                Map.Entry recipe = (Map.Entry) recipesIter.next();
+                this.arecipes.add(new CachedIORecipe(recipe));
+            }
+        } else {
+            super.loadCraftingRecipes(outputId, results);
+        }
+    }
+
+    @Override
+    public void loadCraftingRecipes(ItemStack result) {
+        List recipes = AdvMachinesRecipeManager.getDrainElectrolyzerRecipes();
+        recipes.addAll(AdvMachinesRecipeManager.getPowerElectrolyzerRecipes());
+        Iterator recipesIter = recipes.iterator();
+
+        while (recipesIter.hasNext()) {
+            Map.Entry recipe = (Map.Entry) recipesIter.next();
+            if (NEIServerUtils.areStacksSameTypeCrafting((ItemStack) recipe.getValue(), result)) {
+                this.arecipes.add(new CachedIORecipe(recipe));
+            }
+        }
+    }
+
+    @Override
+    public void loadUsageRecipes(ItemStack ingredient) {
+        List recipes = AdvMachinesRecipeManager.getDrainElectrolyzerRecipes();
+        recipes.addAll(AdvMachinesRecipeManager.getPowerElectrolyzerRecipes());
+        Iterator recipesIter = recipes.iterator();
+
+        while (recipesIter.hasNext()) {
+            Map.Entry recipe = (Map.Entry) recipesIter.next();
+            if (NEIServerUtils.areStacksSameTypeCrafting((ItemStack) recipe.getKey(), ingredient)) {
+                this.arecipes.add(new CachedIORecipe(recipe));
+            }
+        }
+    }
+
+    public class CachedIORecipe extends TemplateRecipeHandler.CachedRecipe {
+        public PositionedStack input;
+        public PositionedStack output;
+
+        public PositionedStack getIngredient() {
+            return this.input;
+        }
+
+        public PositionedStack getResult() {
+            return this.output;
+        }
+
+        public CachedIORecipe(ItemStack itemstack, ItemStack itemstack1) {
+            this.input = new PositionedStack(itemstack, 49, 19);
+            this.output = new PositionedStack(itemstack1, 107, 19);
+        }
+
+        public CachedIORecipe(Map.Entry recipe) {
+            this((ItemStack) recipe.getKey(), (ItemStack) recipe.getValue());
+        }
+    }
+}
