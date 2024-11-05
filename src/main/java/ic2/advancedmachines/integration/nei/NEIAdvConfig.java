b617b75459637539590c5351a38a40d5275e7772
cross <cross.wanted.1.4@gmail.com>
2024-11-05 10:23:03 +0900

    re-added NEI to dev env

--- src/main/java/ic2/advancedmachines/integration/nei/NEIAdvConfig.java
+++ src/main/java/ic2/advancedmachines/integration/nei/NEIAdvConfig.java
@@ -1,45 +1,45 @@
-//package ic2.advancedmachines.integration.nei;
-//
-//import codechicken.nei.api.API;
-//import codechicken.nei.api.IConfigureNEI;
-//import ic2.advancedmachines.AdvancedMachines;
-//import ic2.advancedmachines.integration.nei.providers.NEICompressorProvider;
-//import ic2.advancedmachines.integration.nei.providers.NEIElectrolyzerProvider;
-//import ic2.advancedmachines.integration.nei.providers.NEIExtractorProvider;
-//import ic2.advancedmachines.integration.nei.providers.NEIMaceratorProvider;
-//import ic2.advancedmachines.utils.Refs;
-//import ic2.core.gui.*;
-//
-//public class NEIAdvConfig implements IConfigureNEI {
-//
-//    @Override
-//    public void loadConfig() {
-//        AdvancedMachines.LOGGER.info("Loading NEI Plugin for AdvancedMachines!");
-//
-//        API.registerRecipeHandler(new NEIMaceratorProvider());
-//        API.registerUsageHandler(new NEIMaceratorProvider());
-//        API.registerRecipeHandler(new NEICompressorProvider());
-//        API.registerUsageHandler(new NEICompressorProvider());
-//        API.registerRecipeHandler(new NEIExtractorProvider());
-//        API.registerUsageHandler(new NEIExtractorProvider());
-//
-//        API.registerRecipeHandler(new NEIElectrolyzerProvider());
-//        API.registerUsageHandler(new NEIElectrolyzerProvider());
-//
-//        API.registerGuiOverlay(GuiAdvMacerator.class, "macerator", 5, 11);
-//        API.registerGuiOverlay(GuiAdvCompressor.class, "compressor", 5, 11);
-//        API.registerGuiOverlay(GuiAdvExtractor.class, "extractor", 5, 11);
-//        API.registerGuiOverlay(GuiAdvElectrolyzer.class, "electrolyzer", 0, 0);
-//        API.registerGuiOverlay(GuiAdvInduction.class, "smelting", -4, 11);
-//    }
-//
-//    @Override
-//    public String getName() {
-//        return Refs.NAME;
-//    }
-//
-//    @Override
-//    public String getVersion() {
-//        return Refs.VERSION;
-//    }
-//}
+package ic2.advancedmachines.integration.nei;
+
+import codechicken.nei.api.API;
+import codechicken.nei.api.IConfigureNEI;
+import ic2.advancedmachines.AdvancedMachines;
+import ic2.advancedmachines.integration.nei.providers.NEICompressorProvider;
+import ic2.advancedmachines.integration.nei.providers.NEIElectrolyzerProvider;
+import ic2.advancedmachines.integration.nei.providers.NEIExtractorProvider;
+import ic2.advancedmachines.integration.nei.providers.NEIMaceratorProvider;
+import ic2.advancedmachines.utils.Refs;
+import ic2.core.gui.*;
+
+public class NEIAdvConfig implements IConfigureNEI {
+
+    @Override
+    public void loadConfig() {
+        AdvancedMachines.LOGGER.info("Loading NEI Plugin for AdvancedMachines!");
+
+        API.registerRecipeHandler(new NEIMaceratorProvider());
+        API.registerUsageHandler(new NEIMaceratorProvider());
+        API.registerRecipeHandler(new NEICompressorProvider());
+        API.registerUsageHandler(new NEICompressorProvider());
+        API.registerRecipeHandler(new NEIExtractorProvider());
+        API.registerUsageHandler(new NEIExtractorProvider());
+
+        API.registerRecipeHandler(new NEIElectrolyzerProvider());
+        API.registerUsageHandler(new NEIElectrolyzerProvider());
+
+        API.registerGuiOverlay(GuiAdvMacerator.class, "macerator", 5, 11);
+        API.registerGuiOverlay(GuiAdvCompressor.class, "compressor", 5, 11);
+        API.registerGuiOverlay(GuiAdvExtractor.class, "extractor", 5, 11);
+        API.registerGuiOverlay(GuiAdvElectrolyzer.class, "electrolyzer", 0, 0);
+        API.registerGuiOverlay(GuiAdvInduction.class, "smelting", -4, 11);
+    }
+
+    @Override
+    public String getName() {
+        return Refs.NAME;
+    }
+
+    @Override
+    public String getVersion() {
+        return Refs.VERSION;
+    }
+}
