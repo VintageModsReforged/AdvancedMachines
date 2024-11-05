fc93179c2ebd376e3d34403f42601a59f1498843
cross <cross.wanted.1.4@gmail.com>
2024-11-05 11:13:45 +0900

    removed unused entries

--- src/main/java/ic2/advancedmachines/utils/Refs.java
+++ src/main/java/ic2/advancedmachines/utils/Refs.java
@@ -1,8 +1,5 @@
 package ic2.advancedmachines.utils;
 
-import ic2.advancedmachines.blocks.tiles.base.TileEntityAdvancedMachine;
-import ic2.advancedmachines.blocks.tiles.machines.*;
-
 public class Refs {
 
     public static final String ID = "AdvancedMachines";
@@ -25,20 +22,6 @@ public class Refs {
 
     public static String getGuiPath(String machine) {
         return "/mods/advancedmachines/textures/gui/" + machine + ".png";
-    }
-
-    public static String getTextureName(TileEntityAdvancedMachine tile) {
-        if (tile instanceof TileEntityAdvancedInduction) {
-            return getGuiPath("induction");
-        } else if (tile instanceof TileEntityCentrifugeExtractor) {
-            return getGuiPath("extractor");
-        } else if (tile instanceof TileEntityRotaryMacerator) {
-            return getGuiPath("macerator");
-        } else if (tile instanceof TileEntitySingularityCompressor){
-            return getGuiPath("compressor");
-        } else  if (tile instanceof TileEntityCompactingRecycler) {
-            return getGuiPath("recycler");
-        } else return "";
     }
 
     public static final int AT_MIN_PACKET = 4;
