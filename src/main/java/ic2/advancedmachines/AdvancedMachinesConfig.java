794673fcd2d3a4d185ce8e13a816285d02ebbd0d
CrossVas <cross.wanted.1.4@gmail.com>
2024-11-25 16:40:00 +0900

    removed lang entry from configs

--- src/main/java/ic2/advancedmachines/AdvancedMachinesConfig.java
+++ src/main/java/ic2/advancedmachines/AdvancedMachinesConfig.java
@@ -16,7 +16,6 @@ public class AdvancedMachinesConfig {
     public static final String BLOCK_IDS_CAT = "Block IDs";
 
     public static Configuration MAIN_CONFIG;
-    public static String[] LANGS;
 
     public static int ADV_UPGRADE_ID;
     public static int COMPONENT_ID;
@@ -41,7 +40,6 @@ public class AdvancedMachinesConfig {
         MAIN_CONFIG = new Configuration(new File((File) FMLInjectionData.data()[6], "config/AdvancedMachines.cfg"));
         MAIN_CONFIG.load();
 
-        LANGS = getStrings("language Support", null,"localizations", new String[] {"en_US", "ru_RU"}, "Supported localizations.");
         MACERATOR_WORK_SOUND = getString(SOUND_CAT, SOUND_CAT_DESC, "maceratorWorkSound", "Machines/MaceratorOp.ogg", "Macerator Work Sound");
         COMPRESSOR_WORK_SOUND = getString(SOUND_CAT, SOUND_CAT_DESC, "compressorWorkSound", "Machines/CompressorOp.ogg", "Compressor Work Sound");
         EXTRACTOR_WORK_SOUND = getString(SOUND_CAT, SOUND_CAT_DESC, "extractorWorkSound", "Machines/ExtractorOp.ogg", "Extractor Work Sound");
