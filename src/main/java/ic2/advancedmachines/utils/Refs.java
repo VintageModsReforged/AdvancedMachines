8efdb00a98dd37aa548338795b3a86f30aab50f4
CrossVas <cross.wanted.1.4@gmail.com>
2024-11-25 18:23:39 +0900

    version bump

--- src/main/java/ic2/advancedmachines/utils/Refs.java
+++ src/main/java/ic2/advancedmachines/utils/Refs.java
@@ -4,7 +4,7 @@ public class Refs {
 
     public static final String ID = "AdvancedMachines";
     public static final String NAME = "IC2 Advanced Machines Addon";
-    public static final String VERSION = "4.7.5.2b";
+    public static final String VERSION = "4.7.5.2c";
     public static final String MC_VERSION = "[1.4.7]";
     public static final String DEPS = "required-after:IC2";
 
